1739cebf7082007cdad443e9d7304d438d31b1e3
Juan David <juandaf56@gmail.com>
2020-11-21 08:23:52 -0500

    Estrucura MVC - Framework de proyecto realizado

--- Controllers/Home.php
+++ Controllers/Home.php
@@ -15,36 +15,6 @@
             $this->views->getView($this,"home",$data);
         }
 
-        public function insertar()
-        {
-            $data = $this->model->setUser("Fernando", 25);
-            $data = $this->model->setUser("Alex", 15);
-            print_r($data);
-        }
-
-        public function verusuario($id)
-        {
-            $data = $this->model->getUser($id);
-            print_r($data);
-        }
-
-        public function actualizar(){
-            $data = $this->model->updateUser(1,"Roberto", 20);
-            print_r($data);
-        }
-
-        public function verusuarios()
-        {
-            $data = $this->model->getUsers();
-            print_r($data);
-        }
-
-        public function eliminarusuario($id)
-        {
-            $data = $this->model->delUser($id);
-            print_r($data);
-        }
-
 
     }
 ?>
--- Views/home.php
+++ Views/home.php
@@ -3,19 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $data['page_tag']; ?></title>
+    <title>Document</title>
 </head>
 <body>
-    <?php 
-        dep($data);
-    ?>
-    <section id="<?php echo $data['page_id']; ?>">
-        <h1><?php echo $data['page_title']; ?></h1>
-
-        <p><?php echo $data['page_content']; ?></p>
-
-    </section>
-
-    <?php echo SMONEY.formatMoney(52500.25); ?>
+    
 </body>
 </html>
