3879349de14aa318b36b8a22dd84f6c60809ea12
Juan David <juandaf56@gmail.com>
2020-11-21 08:15:02 -0500

    Avance de MVC 20 Nov

--- Config/Config.php
+++ Config/Config.php
@@ -3,13 +3,23 @@
     //define("BASE_URL", "http://localhost/jlm/");
     const BASE_URL = "http://localhost/jlm/";
 
+    //Zona horaria de Bogotá
+    date_default_timezone_set('America/Bogota');
+
 
     //Datos de conexión a Bases de datos
     const DB_HOST = "localhost";
-    const DB_NAME = "";
+    const DB_NAME = "jlm";
     const DB_USER = "root";
     const DB_PASSWORD = "";
     const DB_CHARSET = "charset=utf8";
+
+    //Separadores decimal y millar Ej: 24,1989.00
+    const SPD = ".";
+    const SPM = ".";
+
+    //Simbolo de la moneda
+    const SMONEY = "COP ";
 
 
 ?>
--- Views/home.php
+++ Views/home.php
@@ -3,14 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $data['tag_page']; ?></title>
+    <title><?php echo $data['page_tag']; ?></title>
 </head>
 <body>
+    <?php 
+        dep($data);
+    ?>
     <section id="<?php echo $data['page_id']; ?>">
         <h1><?php echo $data['page_title']; ?></h1>
 
         <p><?php echo $data['page_content']; ?></p>
 
     </section>
+
+    <?php echo SMONEY.formatMoney(52500.25); ?>
 </body>
 </html>
